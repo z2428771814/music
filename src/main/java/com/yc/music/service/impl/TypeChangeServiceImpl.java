fd79b0107cf1113d68f36adf11b4517bd2c781ec
z2428771814 <2428771814@qq.com>
2019-10-31 21:35:59 +0800

    分词查询

--- src/main/java/com/yc/music/service/impl/TypeChangeServiceImpl.java
+++ src/main/java/com/yc/music/service/impl/TypeChangeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yc.music.service.impl;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.ListModel;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,15 +26,18 @@ import com.yc.music.bean.SingerInfo;
 import com.yc.music.bean.SongTypeInfo;
 import com.yc.music.mapper.ITypeChangeMapper;
 import com.yc.music.service.ITypeChangeService;
+import com.yc.music.util.Participle;
 import com.yc.music.util.StringUtil;
 
 @Service
 public class TypeChangeServiceImpl implements ITypeChangeService {
-	
+	private  int id=0;
+
+
 	@Autowired
 	private ITypeChangeMapper mapper;
 
-	
+
 	public int addApplicationType(ApplicationTypeInfo alt) {
 		if( StringUtil.checkNull( alt.getAtname() ) ){
 			return -1;
@@ -108,7 +114,7 @@ public class TypeChangeServiceImpl implements ITypeChangeService {
 			pageNo = 0;
 		}
 		map.put("pageNo", pageNo);
-		
+
 		map.put("pageSize", pageSize);
 		return mapper.pagingSingerInfo(map);
 	}
@@ -170,45 +176,72 @@ public class TypeChangeServiceImpl implements ITypeChangeService {
 			}else{
 				return -1;
 			}
-		}else{
-			return -1;
 		}
 		System.out.println( musicinfo );
+		id=-1;
 		return mapper.addMusicInfo(musicinfo);
 	}
 
 
 	@Override
-	public List<Map<String, Object>> findMusicInfo(Integer pageNo,Integer pageSize,String mname) {
+	public List<Map<String, Object>> findMusicInfo(Integer pageNo,Integer pageSize,String mname,HttpServletRequest request) {
 		Map<String, Object> map=new HashMap<String,Object>();
 		if( pageNo == 0 && pageSize == 0 ){
 			if( mname == "" || mname.length()<=0  ){
 				map.put("pageNo", null);
 				map.put("pageSize", null);
-				map.put("mname", null);
-				return mapper.findMusicInfo(map);
+				List<Map<String, Object>> list=mapper.findMusicInfo(map);
+				return list;
 			}else{
-				map.put("mname", mname);
-				map.put("pageNo", null);
-				map.put("pageSize", null);
-				return mapper.findMusicInfo(map);
+				//				map.put("mname", mname);
+				//				map.put("pageNo", null);
+				//				map.put("pageSize", null);
+				//				return mapper.findMusicInfo(map);
+				try {
+					List<Map<String, Object>> list=Participle.searchIndexMusciInfo(mname);
+
+					return list;
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InvalidTokenOffsetsException e) {
+					e.printStackTrace();
+				}
+				return null;
+
+
 			}
-			
+
 		}else{
 			if( mname == "" || mname.length()<=0  ){
 				map.put("pageNo", (pageNo-1)*pageSize);
 				map.put("pageSize", pageSize);
-				map.put("mname", null);
 				return mapper.findMusicInfo(map);
+
 			}else{
-				map.put("mname", mname);
-				map.put("pageNo", (pageNo-1)*pageSize);
-				map.put("pageSize", pageSize);
-				return mapper.findMusicInfo(map);
+				List<Map<String, Object>> list=null;
+				try {
+					list=Participle.searchIndexMusciInfo(mname);
+					if( ((pageNo-1)*pageSize+pageSize)>=(list.size()-1) ){
+						System.out.println( (pageNo-1)*pageSize+"---"+list.size() );
+						return list.subList((pageNo-1)*pageSize, list.size());
+					}
+					System.out.println( "我们的值为---"+list );
+					return list.subList((pageNo-1)*pageSize, (pageNo-1)*pageSize+pageSize);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InvalidTokenOffsetsException e) {
+					e.printStackTrace();
+				}
 			}
-			
+
 		}
-		
+		return null;
+
+
 	}
 
 
@@ -218,12 +251,13 @@ public class TypeChangeServiceImpl implements ITypeChangeService {
 		String path = request.getServletContext().getRealPath(""); //这个是获取服务器路径  D:\tomcat\apache-tomcat-8.5.41\webapps\music\
 		path=path.substring(0,(path.length()-6));
 		List<CombinationInfo> com=this.findCombinationInfo();
-		
-		
+
+
 		//这个是查询全部
-		List<Map<String, Object>> mu=this.findMusicInfo(0,0,mname);
+		List<Map<String, Object>> mu=this.findMusicInfo(0,0,mname,request);
 		//这个是首页的分页查询
-		List<Map<String, Object>> music=this.findMusicInfo(pageNo,pageSize,mname);
+		List<Map<String, Object>> music=this.findMusicInfo(pageNo,pageSize,mname,request);
+		System.out.println( "查询出来的集合为"+music );
 		for( int i=0;i<music.size();i++ ){
 			int cid= (int) music.get(i).get("cid");
 			if( cid != 0 ){
@@ -235,9 +269,10 @@ public class TypeChangeServiceImpl implements ITypeChangeService {
 				}
 			}
 			if( music.get(i).get("lyricaddr") != null && music.get(i).get("lyricaddr") != ""){
-				path+=music.get(i).get("lyricaddr");
+				String musicPath=path+music.get(i).get("lyricaddr");
 				try {
-					InputStream is = new FileInputStream(path);
+					System.out.println( "musicPath"+musicPath );
+					InputStream is = new FileInputStream(musicPath);
 					byte[] b = new byte[is.available()];//把所有的数据读取到这个字节当中
 					//完整的读取一个文件
 					is.read(b);
@@ -250,10 +285,10 @@ public class TypeChangeServiceImpl implements ITypeChangeService {
 					e.printStackTrace();
 				}
 			}
-			
+
 		}
-		
-		
+
+
 		Map<String, Object> map=new HashMap<String,Object>();
 		map.put("total", mu.size());
 		map.put("music", music);
@@ -263,7 +298,32 @@ public class TypeChangeServiceImpl implements ITypeChangeService {
 
 	@Override
 	public int deleteMusicInfo(MusicInfo musicInfo) {
+		id=-1;
 		return mapper.deleteMusicInfo(musicInfo);
+	}
+
+
+	@Override
+	public List<Map<String, Object>> findpadd(HttpServletRequest request) {
+		if( id==0 ){
+			Map<String, Object> map=new HashMap<String,Object>();
+			map.put("pageNo", null);
+			map.put("pageSize", null);
+			List<Map<String, Object>> list=mapper.findMusicInfo(map);
+			Participle.addTo(list, request);
+			++id;
+			return list;
+		}else if( id == -1 ){
+			String tompath = request.getServletContext().getRealPath(""); //这个是获取服务器路径  D:\tomcat\apache-tomcat-8.5.41\webapps\music\
+			tompath=tompath.substring(0,(tompath.length()-6));
+			tompath=tompath.replace("\\", "\\")+"index";
+			File file=new File(tompath);
+			Participle.delete(file);
+			id=0;
+			return null;
+		}
+		return null;
+		
 	}
 
 
